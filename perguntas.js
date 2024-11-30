4343f6473bb2681c0e33fc6b161c72b2d6844db8
Matheus <cauadesouzamatheus@gmail.com>
2024-11-29 23:47:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'História',
+    'Qual o ano de inicio da primeira guerra mundial?',
+    'O ano de inicio foi 1914.'
+)
+
+criaCartao (
+    'História',
+    'O que foi a Guerra Fria?',
+    'Foi um período de conflito político, militar, econômico, tecnológico, social e ideológico'
+)
+
+criaCartao (
+    'Geografia',
+    'Quantas provincias tem a Russia?',
+    'A Rússia tem 46 províncias, chamadas de oblasts, que são o tipo mais comum de súditos federais do país.'
+)
+
+criaCartao (
+    'História',
+    'O que foi a Corrida Espacial?',
+    'Foi uma competição tecnológica entre os Estados Unidos e a União Soviética, que ocorreu entre 1957 e 1975'
+)
